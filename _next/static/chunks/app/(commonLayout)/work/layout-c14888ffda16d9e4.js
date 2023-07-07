14752327ec4a90fe53770a8378728e03798b9fd5
koh110 <koh110@users.noreply.github.com>
2023-07-07 09:24:51 +0000

    deploy: a1d17b8de2be5fcf67fbf2f221a6c4ed3a12463a

---
+++ _next/static/chunks/app/(commonLayout)/work/layout-c14888ffda16d9e4.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[584],{8752:function(e,r,t){Promise.resolve().then(t.bind(t,3575))},3575:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return c}});var n=t(9268),s=t(5846),o=t.n(s),i=t(6008),l=t(6666),f=t.n(l);function u(e){let{children:r}=e;return(0,n.jsx)("div",{className:f().title,children:r})}function c(e){let{children:r}=e,t=(0,i.usePathname)(),s={opacity:.3},l=t.includes("/work/log"),f=l?{...s,paddingLeft:"1em"}:{paddingLeft:"1em"},c=l?void 0:{...s};return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(u,{children:"Work"}),(0,n.jsxs)("div",{style:{display:"flex",justifyContent:"flex-end",padding:"0 1em"},children:[(0,n.jsx)(o(),{href:"/work",passHref:!0,shallow:!0,style:c,children:"Work"}),(0,n.jsx)(o(),{href:"/work/log",passHref:!0,shallow:!0,style:f,children:"Log"})]}),r]})}},6666:function(e){e.exports={title:"styles_title__tXC9q"}},3177:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(6006),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,l=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function u(e,r,t){var n,o={},u=null,c=null;for(n in void 0!==t&&(u=""+t),void 0!==r.key&&(u=""+r.key),void 0!==r.ref&&(c=r.ref),r)i.call(r,n)&&!f.hasOwnProperty(n)&&(o[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===o[n]&&(o[n]=r[n]);return{$$typeof:s,type:e,key:u,ref:c,props:o,_owner:l.current}}r.Fragment=o,r.jsx=u,r.jsxs=u},9268:function(e,r,t){"use strict";e.exports=t(3177)}},function(e){e.O(0,[487,253,769,744],function(){return e(e.s=8752)}),_N_E=e.O()}]);
